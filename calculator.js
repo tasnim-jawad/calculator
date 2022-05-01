ec397f4a5cdc04eae8ee536a00f744b5d2688ec9
tasnim-jawad <muhammadtasnimulhasan1@gmail.com>
2022-05-02 03:15:55 +0600

    clear and backspace operator active

--- calculator.js
+++ calculator.js
@@ -11,7 +11,7 @@ function getOutput() {
 }
 
 function printOutput(num) {
-    if (num == "") {
+    if (num == "") {  //number na thakle jate 0 na dekhay.khalithakle jate khali dekhay
         document.getElementById("output-value").innerText = num;
     }else{
         document.getElementById("output-value").innerText = getFormattedNumber(num);  //ekhane output-e nuber print kore display-te dekhachche
@@ -28,5 +28,23 @@ printOutput("5435435345")
 
 function reverseNumberFormet(num) {  //jokhon koma uthay nite hobe tokhon ei function er vitor diye number nite hobe
     return Number(num.replace(/,/g,'')); 
+}
+
+let operator = document.getElementsByClassName("operator")
+for (let i = 0; i < operator.length; i++) {
+    const element = operator[i];
+    element.addEventListener('click' , function() {
+        if(this.id == "clear"){
+            printHistory("");
+            printOutput("")
+        }else if(this.id == "backspace"){
+            let output =  reverseNumberFormet(getOutput()).toString(); //output niye koma uthano hoyeche ebong number ke  string banano hochche
+            
+            if(output){                                   //jodi etar man thake
+                output = output.slice(0,output.length-1);      //output jeta peyechi setar 0 index theke shuru kore shesher agerta porjonto kete nibe
+                printOutput(output)                                 //notun output print koredibe
+            }
+        }
+    });
 }
 
